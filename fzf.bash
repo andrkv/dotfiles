e29348435336bf133e54d539d6dd0172dd8140fa
Andrey Kovalevskiy <andr.kovalevskiy@gmail.com>
2021-03-09 21:02:04 +0200

    Add fdfind call if fd is failed
    Use color always for bat

--- fzf.bash
+++ fzf.bash
@@ -12,14 +12,17 @@ fi
 # ------------
 source "$HOME/.fzf/shell/key-bindings.bash"
 
-export FZF_DEFAULT_OPTS="--no-mouse --height=50% --reverse --multi --inline-info
+export FZF_DEFAULT_OPTS="--no-mouse --height=40% --reverse --multi --inline-info
   --preview='[[ \$(file --mime {}) =~ binary ]] && echo ''No preview'' ||
-    (bat{} || cat {}) 2>/dev/null | head -100'
+    (bat {} --color=always || cat {}) 2>/dev/null | head -100'
   --preview-window='hidden:right:70%'
   --bind=ctrl-s:toggle-preview
   --bind=ctrl-d:half-page-down,ctrl-u:half-page-up
   --bind=ctrl-e:preview-down,ctrl-y:preview-up"
 
-export FZF_DEFAULT_COMMAND="(git ls-files --exclude-standard || fd || find) 2> /dev/null"
+FD_DEFAULT_OPTIONS="--type f --hidden --exclude .git"
+export FZF_DEFAULT_COMMAND="(fd $FD_DEFAULT_OPTIONS ||
+                             fdfind $FD_DEFAULT_OPTIONS ||
+                             find) 2> /dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type d || find -type d"
