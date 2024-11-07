21b8f34327a7243f83c6e7c838ddbf8c5bc87377
Andrey Kovalevskiy <andr.kovalevskiy@gmail.com>
2024-11-07 16:12:29 +0200

    Setup llvm repos

--- setup.sh
+++ setup.sh
@@ -5,9 +5,18 @@ set -e
 PREFIX=${HOME}/.local
 SRC=${HOME}/sources
 
+if [ -z "$(grep "llvm-toolchain" /etc/apt/sources.list)" ]; then
+    echo -e '\ndeb http://apt.llvm.org/focal/ llvm-toolchain-focal-18 main' |\
+        sudo tee -a /etc/apt/sources.list > /dev/null
+    echo 'deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-18 main' |\
+        sudo tee -a /etc/apt/sources.list > /dev/null
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+fi
+
 sudo apt update && sudo apt upgrade
 
-sudo apt install gcc python3-dev libncurses-dev make ripgrep fd-find curl tmux
+sudo apt install gcc python3-dev libncurses-dev make ripgrep fd-find curl tmux \
+    clangd-18 batcat
 
 if [ ! -d "${HOME}/.fzf" ]; then
     git clone https://github.com/junegunn/fzf ${HOME}/.fzf
@@ -46,5 +55,7 @@ cp vimrc        ${HOME}/.vimrc
 cp tmux.conf    ${HOME}/.tmux.conf
 cp fzf.bash     ${HOME}/.fzf.bash
 cp bash_aliases ${HOME}/.bash_aliases
+
+ln -s /usr/bin/batcat ${PREFIX}/bin/bat
 
 echo "Please source profile and bashrc, 'source ~/.profile && source ~/.bashrc'"
